f02523e5c5b2a517d9c720415735c34a7a26642c
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2022-02-27 12:22:59 -0500

    Primer proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Hola from './componentes/hola';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Titulo del primer proyecto</h1>
+     <Hola></Hola>
+    </div>
+  );
+}
+
+export default App; 
---
+++ src/componentes/hola.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Hola() {
+  return (
+    <div>
+     <h2>Primer componente hola</h2>
+    </div>
+  )
+}
+
+export default Hola;
