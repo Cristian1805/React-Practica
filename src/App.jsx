b28ee85ea144eae701b8540779b6a070f9bc3b0c
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2022-07-06 22:54:59 -0500

    Añadidos list key map y operador ternario para la temperatura

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
 import  Contador  from './componentes/Contador';
+import Listado from './componentes/Listado';
+import Temperatura from './componentes/Temperatura';
 
 function App() {
   return (
     <div className="App">
      <h1>Titulo del primer proyecto</h1>
      <Contador></Contador>
-     <Contador></Contador>
-     <Contador></Contador>
+     <Listado></Listado>
+     <Temperatura> </Temperatura>
     </div>
   );
 }
